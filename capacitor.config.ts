dc1aa9ece608782bc81ee9faa2d1b851a8fd1f28
Alan Christian Ramírez Rodríguez <ingenieroacramirez@gmail.com>
2025-06-23 18:49:16 -0600

    Enhance Capacitor configuration to enable HttpClient plugin and update main.ts to provide HttpClient for the application.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,15 @@
 import type { CapacitorConfig } from '@capacitor/cli';
+import { HttpClient } from '@angular/common/http';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'nginx-financing-hibryd',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    HttpClient: {
+      enabled: true,
+    },
+  },
 };
 
 export default config;
