3e8dea2a98b44bdd5bf5cabc86a353605afdaace
Alan Christian Ramírez Rodríguez <ingenieroacramirez@gmail.com>
2025-06-23 22:02:43 -0600

    Add ngx-spinner for loading indicators; update Angular configuration and main.ts to include animations. Refactor auth page to show spinner during authentication process.

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -1,20 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.page.html',
   styleUrls: ['./auth.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    NgxSpinnerModule,
+  ],
 })
 export class AuthPage implements OnInit {
+  private spinner = inject(NgxSpinnerService);
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    // Mostrar spinner
+    this.spinner.show();
 
+    // Simular proceso de autenticación (para testing)
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 3000);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ import {
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -20,5 +21,6 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(),
+    provideAnimations(),
   ],
 });
